c1961924d608abee1185fb99b10657321a493ae9
hossa <hossam.spiderman94@gmail.com>
2021-09-17 21:58:19 +0200

    Forth Commit

--- app/src/main/java/com/example/cloud/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/cloud/ui/login/LoginFragment.kt
@@ -2,19 +2,19 @@ package com.example.cloud.ui.login
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.cloud.R
 import com.example.cloud.api.LoggingOperation
 import com.example.cloud.databinding.FragmentLoginBinding
-import com.example.cloud.viewModels.ViewModelFactory
 import com.example.cloud.viewModels.LoginViewModel
+import com.example.cloud.viewModels.ViewModelFactory
 
 
 class LoginFragment : Fragment() {
@@ -35,7 +35,7 @@ class LoginFragment : Fragment() {
         viewModelFactory = ViewModelFactory(application)
         viewModel = ViewModelProvider(this, viewModelFactory).get(LoginViewModel::class.java)
         binding.lifecycleOwner = this
-        binding.newaccount.setOnClickListener{
+        binding.newaccount.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_createAccountFragment)
         }
         binding.signin.setOnClickListener {
@@ -51,31 +51,33 @@ class LoginFragment : Fragment() {
                     binding.password.text.toString().trim()
                 )
             }
-
-            //Init your Observers
-            viewModel.loginSuccess.observe(viewLifecycleOwner, {
-                when {it.equals(LoggingOperation.Ok) -> { findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
-                        Toast.makeText(context, "Logged in", Toast.LENGTH_LONG).show()
-                    }it.equals(LoggingOperation.Failed) -> {
-                        findNavController().popBackStack()
-                        Toast.makeText(context, "Login failed", Toast.LENGTH_LONG).show()
-                    }else -> {
-                        Log.v("op",it.toString())
-                    }
-                }
-            })
-            viewModel.spinner.observe(viewLifecycleOwner, {
-                if (it) {
-                    binding.progressBar2.visibility = View.VISIBLE
-                    binding.signin.visibility = View.INVISIBLE
-                }
-                else {
-                    binding.progressBar2.visibility = View.INVISIBLE
-                    binding.signin.visibility = View.VISIBLE
-                }
-            })
-
         }
+
+        //Init your Observers
+        viewModel.loginSuccess.observe(viewLifecycleOwner, {
+            when {
+                it.equals(LoggingOperation.Ok) -> {
+                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                    Toast.makeText(context, "Logged in", Toast.LENGTH_LONG).show()
+                }
+                it.equals(LoggingOperation.Failed) -> {
+                    Toast.makeText(context, "Login failed", Toast.LENGTH_LONG).show()
+                }
+                else -> {
+                    Log.v("op", it.toString())
+                }
+            }
+        })
+        viewModel.spinner.observe(viewLifecycleOwner, {
+            if (it) {
+                binding.progressBar2.visibility = View.VISIBLE
+                binding.signin.visibility = View.INVISIBLE
+            } else {
+                binding.progressBar2.visibility = View.INVISIBLE
+                binding.signin.visibility = View.VISIBLE
+            }
+        })
+
 
         return binding.root
     }
